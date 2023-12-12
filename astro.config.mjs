86c3dafa2bbd28ab38f4bd0388599107d3097e7f
1tpp <panapat.dev@gmail.com>
2023-12-12 15:14:20 +0700

    chore: update vercel add-on and optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+// import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.1tpp.dev",
-  integrations: [preact()]
+  integrations: [preact()],
+  // output: "server", // uncomment this line to use server-side rendering
+  output: 'static',
+  adapter: vercel({
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+    },
+  }),
 });
