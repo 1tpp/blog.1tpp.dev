1fb901690f7c434f2dc7d6eff1ab0517e4e393c4
1tpp <panapat.dev@gmail.com>
2023-12-14 01:24:13 +0700

    chore: update content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,21 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const blogs = await getCollection('blogs');
+  const items =  blogs.map((blog) => ({
+    // title: blog.data.title,
+    // pubDate: blog.data.date,
+    // description: blog.data.description,
+    ...blog.data,
+    link: `/blogs/${blog.slug}/`,
+  }));
+
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
+    site: context.site,
+    items,
   });
 }
